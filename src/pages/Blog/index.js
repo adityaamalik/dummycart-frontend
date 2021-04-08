fc1868c37f3fc776732f1293ce09d99704cf7320
Aditya Malik <aditya007@outlook.com>
2021-04-08 19:42:50 +0530

    blog section improved

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -16,8 +16,8 @@ const Blog = () => {
         <Row justify="center">
           <Col lg={12} md={24} sm={24} xs={24}>
             <Image
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              style={{ height: "400px", width: "auto" }}
+              src="http://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8cHJvZHVjdHx8MHx8fHwxNjE3NjU4NTM2&ixlib=rb-1.2.1&q=80&w=1080"
+              style={{ height: "auto", width: "90%" }}
             />
           </Col>
           <Col lg={12} md={24} sm={24} xs={24}>
--- src/pages/Blogs/index.js
+++ src/pages/Blogs/index.js
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Row, Col } from "antd";
 import * as S from "./styles";
 
 const { Meta } = Card;
@@ -19,18 +19,23 @@ const Blogs = () => {
           onClick={() => (window.location.pathname = "/blog/123")}
           style={{ marginBottom: "10px" }}
           hoverable
-          cover={
-            <img
-              style={{ objectFit: "cover", width: "100%", height: "auto" }}
-              alt="Blog Heading"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            />
-          }
         >
-          <Meta
-            title="Blog Heading"
-            description="First few lines of the blog"
-          />
+          <Row>
+            <Col span={8}>
+              <img
+                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+                alt="Blog Heading"
+                src="http://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8cHJvZHVjdHx8MHx8fHwxNjE3NjU4NTM2&ixlib=rb-1.2.1&q=80&w=1080"
+              />
+            </Col>
+            <Col span={1}></Col>
+            <Col span={15}>
+              <Meta
+                title="Blog Heading"
+                description="First few lines of the blog"
+              />
+            </Col>
+          </Row>
         </Card>
       </S.Container>
     </>
